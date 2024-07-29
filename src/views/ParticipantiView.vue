1972c1f4d1a121e64e22c5178a2c069f7e10e853
Costeiu Iulian Alexandru <costeiu_iulian@yahoo.com>
2024-07-29 15:45:35 +0300

    fix iframe

--- src/views/ParticipantiView.vue
+++ src/views/ParticipantiView.vue
@@ -3,14 +3,12 @@ import { db } from '../firebase.js';
 import { getDocs, collection } from 'firebase/firestore';
 import { ref, onMounted } from 'vue';
 
+</script>
+
 <template>
   <div>
-    <!-- Link to the registration page -->
-    <a href="https://racehub.ro/register/trc2024?language=ro-RO">aici</a>
     <br>
-    <!-- Embedded registration form -->
-    <iframe style="overflow: hidden;" src="https://racehub.ro/register/trc2024?language=ro-RO" width="100%" height="3100px" frameborder="0"></iframe>
-    <!-- Embedded participant list -->
+     <!-- Embedded participant list -->
     <iframe style="overflow: hidden;" src="https://racehub.ro/register/trc2024/list?language=ro-RO" width="100%" height="3100px" frameborder="0"></iframe>
   </div>
 </template>
