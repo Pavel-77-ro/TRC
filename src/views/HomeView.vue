28b3a56bc57f1c705fade59fe472ccc5d5339428
Costeiu Iulian Alexandru <costeiu_iulian@yahoo.com>
2024-12-08 10:52:40 +0200

    update text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -119,12 +119,12 @@ onMounted(() => {
         </h2>
         <h2
          class="text-orange-400 text-2xl sm:text-3xl lg:text-4xl text-center lg:text-left mb-6 font-Lucky">
-         22 Septembrie
+         08 Iunie
         </h2>    
         <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
-          Înscrie-te la Turnu Roșu Challenge până pe
-          <span class="text-orange-400">15 septembrie!</span> Explorează trasee montane uimitoare la
-          a doua ediție a evenimentului nostru de alergare și hiking!
+          Înscrie-te la Turnu Roșu Challenge începând cu
+          <span class="text-orange-400">01 ianuarie!</span> Explorează trasee montane uimitoare la
+          cea de a treia ediție a evenimentului nostru de alergare și hiking!
         </p>
 
         <!-- Countdown Timer -->
