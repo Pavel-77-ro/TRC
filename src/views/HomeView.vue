8a1b1e6dd9b0cd20b05db83453e74973bc4bbdf1
Costeiu Iulian Alexandru <costeiu_iulian@yahoo.com>
2025-02-12 22:11:02 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -122,7 +122,7 @@ onMounted(() => {
           Turnu Rosu Challenge
         </h2>
         <h3
-          class="text-[#cc1812] text-3xl sm:text-4xl lg:text-5xl text-center lg:text-left mb-6 font-Lucky">
+          class="text-[#cc1812] text-2xl sm:text-3xl lg:text-4xl text-center lg:text-left mb-6 font-Lucky">
           08 iunie 2025
         </h3>
         <h2
