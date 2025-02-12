c6c52f0472dc5582e0a672c37dc6d53f39eac468
Costeiu Iulian Alexandru <costeiu_iulian@yahoo.com>
2025-02-12 21:35:25 +0200

    updated the main partner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -122,8 +122,8 @@ onMounted(() => {
           Turnu Rosu Challenge
         </h2>
         <h2
-         class="text-blue-400 text-2xl sm:text-3xl lg:text-4xl text-center lg:text-left mb-6 font-Lucky">
-         08 Iunie
+         class="text-[#00A0E3]-400 text-2xl sm:text-3xl lg:text-4xl text-center lg:text-left mb-6 font-Lucky">
+         Powered by Bilstein
         </h2>    
         <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
           Înscrie-te la Turnu Roșu Challenge începând cu
